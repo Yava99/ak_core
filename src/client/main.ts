97e658bc961e44667d5737e9d5eca62fd0bcff12
Yava99 <158023003+Yava99@users.noreply.github.com>
2026-06-28 09:52:09 +0200

    Delete src directory

--- src/client/main.ts
+++
@@ -1,8 +0,0 @@
-import { Events } from '../shared/events';
-
-console.log('[ak_core] Client script loaded');
-
-on('playerSpawned', () => {
-  console.log('[ak_core] playerSpawned event, notifying server...');
-  emitNet(Events.core.playerLoaded);
-});
