36265cc95431462d2fe322e4828e1ae0bbf36e64
Yava99 <158023003+Yava99@users.noreply.github.com>
2026-06-28 10:11:47 +0200

    Delete packages/core-errors/dist directory

--- packages/core-errors/dist/domain/domain-error.d.ts
+++
@@ -1,5 +0,0 @@
-import { AppError } from "../base/app-error";
-import type { ErrorDetails } from "../types/error-details";
-export declare class DomainError extends AppError {
-    constructor(message: string, details?: ErrorDetails, cause?: unknown);
-}
--- packages/core-errors/dist/infrastructure/infrastructure-error.d.ts
+++
@@ -1,5 +0,0 @@
-import { AppError } from "../base/app-error";
-import type { ErrorDetails } from "../types/error-details";
-export declare class InfrastructureError extends AppError {
-    constructor(message: string, details?: ErrorDetails, cause?: unknown);
-}
